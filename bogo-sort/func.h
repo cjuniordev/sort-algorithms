268e4582f002a595b10a5366713cf4a4e775b5cd
Nicolas Resende <nicolascampos1093@gmail.com>
2021-12-22 17:20:51 -0300

    feat: added bogo sort

--- bogo-sort/func.h
+++ bogo-sort/func.h
@@ -1,3 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 // cabeçalho das funções -----
+int estaOrdenado(int *lista, int n, int **i);
+void ordenar(int *lista, int n);
+void bogoSort(int *lista, int n, int *i);
--- bogo-sort/main.c
+++ bogo-sort/main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "func.h"
 
 int main()
 {
-    printf("Hello World");
+  int lista[] = { 4, 1, 3, 7, 8, 10, -5 };
+  int comprimento = sizeof(lista)/sizeof(lista[0]);
+  int num = 0;
 
-    return 0;
+  printf("Lista Original:\n");
+
+  for (int i = 0; i < comprimento; i++)
+  {
+    printf("%d%s", lista[i], i == comprimento - 1 ? "\n" : " ");  
+  }
+
+  printf("\nLista Organizada:\n");
+
+  bogoSort(lista, 7, &num);
+
+  for (int i=0; i < 7; i++)
+  {
+    printf("%d ", lista[i]);
+  }
+
+  printf("\n Numero de vezes que o Metodo de Ordenacao foi usado: %d", num);
 }
