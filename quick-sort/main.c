2fc1ce77b660f4b17a53220b3f541182321e7a88
Nicolas Resende <nicolascampos1093@gmail.com>
2021-12-23 13:36:10 -0300

    fix: do you like jazz?

--- quick-sort/main.c
+++ quick-sort/main.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-    int list[] = {2, 4, 6, 7, 9, 1, 4, 6, 9, 0}; // declarando lista desordenada
-    int len = sizeof(list)/sizeof(list[0]); //Variavel contendo o comprimento da lista
+    int list[] = {2, 4, 6, 7, 9, 1, 4, 6, 9, 0}; // Declarando lista desordenada
+    int len = sizeof(list)/sizeof(list[0]); // Variavel contendo o comprimento da lista
 
     printf("Lista desordenada:\n");
-    
+
     imprimeLista(list, len);
     
-    quickSort(list, 0, len); // ordena lista
+    quickSort(list, 0, len); // Chamada da função que aplica o metodo bogosort
 
     printf("\nLista ordenada:\n");
 
