caf6a6c27cc26fb980af043b472758963dde5a82
cjuniordev <carlosrogeriocr02@gmail.com>
2021-12-24 11:09:57 -0300

    fix: radix acess unknown index in array

--- radix-sort/func.h
+++ radix-sort/func.h
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // cabeçalho das funções -----
-void radixSort(int list[], int len);
+int pegaDigito(int numero, int divisor);
+int maximo(int lista[], int n);
+void zeraLista(int *lista, int n);
+void countingSort(int *lista, int n, int divisor, int *aux);
+void radixSort(int *lista, int n);
 void imprimeLista(int *lista, int n);
--- radix-sort/main.c
+++ radix-sort/main.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-  int list[] = { 4, 1, 3, 7, 8, 10, -5 }; //Variavel contendo a lista
+  int list[] = { 4, 1, 3, 7, 8, 10, -5, 68 }; //Variavel contendo a lista
   int len = sizeof(list)/sizeof(list[0]); //Variavel contendo o comprimento da lista
 
   printf("Lista Desordenada:\n");
@@ -13,7 +13,7 @@ int main()
 
   printf("\nLista Ordenada:\n");
 
-  radixSort(list, len); //Chamada da função que aplica o metodo radixSort
+  radix_sort(list, len); //Chamada da função que aplica o metodo radixSort
 
   imprimeLista(list, len);
 
