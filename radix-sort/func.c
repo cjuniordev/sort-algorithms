c41a851163d2767732f8e66401e3fa6a3773419e
cjuniordev <carlosrogeriocr02@gmail.com>
2021-12-24 13:25:52 -0300

    feat: working with negative numbers

--- radix-sort/func.c
+++ radix-sort/func.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "func.h"
 
 int pegaDigito(int numero, int divisor){
@@ -15,12 +16,34 @@ int maximo(int lista[], int n){ // lista e tamanho
     return max;
 }
 
-void zeraLista(int *lista, int n){
+void juntaVetores(int *v, int *inicial, int *final, int n){ // concatena dois vetores
+    for (int i = 0; i < n; i++) v[i] = inicial[i];
+    for (int i = 0; i < n; i++) v[i + n] = final[i];
+}
+
+void negativaLista(int *lista, int n){ // deixa todos elementos negativos
+    for(int i = 0; i < n; i++) {
+        lista[i] = (-1)*lista[i];
+    }
+}
+
+void inverteLista(int *lista, int n){ // inverte a ordem da lista
+    int invertido[n];
+    for(int i = 0; i < n; i++) {
+        invertido[i] = lista[(n-i)-1];
+    }
+    for(int i=0; i<n; i++){
+        lista[i] = invertido[i];
+    }
+}
+
+void zeraLista(int *lista, int n){ // define todos elementos de uma lista como sendo 0
     for(int i=0; i<n; i++){
         lista[i] = 0;
     }
 }
 
+// NAO ALTERA NADA NESSA FUNCAO, O ERRO NAO TA AQUI
 void countingSort(int *lista, int n, int divisor, int *aux){
     int base = 10;
     int digito, c[base], s = 0; // t --> soma de prefixo || c[base] --> lista de contagem
@@ -53,12 +76,47 @@ void radixSort(int *lista, int n){
     int divisor = 1;
     int q = maximo(lista, n); // pega o maior numero do vetor
     int *aux = (int *)calloc(n, sizeof(int));
-    while(q > 0){
+    int *negativo = (int *)calloc(n, sizeof(int));
+
+    zeraLista(negativo, n);
+    for(int i=0; i<n; i++){ // pega todos elementos negativos da lista original, copia para outra lista pega o abs
+        if(lista[i]<0){
+            negativo[i] = (-1)*lista[i];
+            lista[i] = (-1)*lista[i];
+        }
+    }
+
+    while(q > 0){ // ordena lista dos positivos
         countingSort(lista, n, divisor, aux);
+        divisor *= 10; // divisor multiplo de 10 para ir pegando sempre a proxima casa decimal
+        q /= 10; // diminui a quantidade de casas do maior numero
+    }
+
+    // seta variaveis novamente
+    divisor = 1;
+    q = maximo(negativo, n); // pega o maior numero do vetor
+    int *aux2 = (int *)calloc(n, sizeof(int));
+    while(q > 0){ // ordena negativos
+        countingSort(negativo, n, divisor, aux2);
         divisor *= 10; // dividor multiplo de 10 para ir pegando sempre a proxima casa decimal
         q /= 10; // diminui a quantidade de casas do maior numero
     }
+    inverteLista(negativo, n); // deixa a lista decrescente
+    negativaLista(negativo, n); // deixa os numeros negativos
+
+    int *aux3 = (int *)calloc(n*2, sizeof(int));
+    juntaVetores(aux3, negativo, lista, n); // junta as duas listas em uma so
+
+    for(int i=0, j=0; i<(n*2); i++){ // remove os zeros e retorna à lista original
+        if(aux3[i] != 0){
+            lista[j] = aux3[i];
+            j++;
+        }
+    }
+
     free(aux); // libera memoria
+    free(aux2); // libera memoria
+    free(aux3); // libera memoria
 }
 
 void imprimeLista(int *lista, int n) // Funcao que imprime a lista, para evitar repetição de codigo
--- radix-sort/func.h
+++ radix-sort/func.h
@@ -4,6 +4,9 @@
 // cabeçalho das funções -----
 int pegaDigito(int numero, int divisor);
 int maximo(int lista[], int n);
+void juntaVetores(int *v, int *inicial, int *final, int n);
+void negativaLista(int *lista, int n);
+void inverteLista(int *lista, int n);
 void zeraLista(int *lista, int n);
 void countingSort(int *lista, int n, int divisor, int *aux);
 void radixSort(int *lista, int n);
