b559d5b020cda5a917d526af32b51af017d11dba
cjuniordev <carlosrogeriocr02@gmail.com>
2021-12-30 12:45:37 -0300

    feat: add experiment 1

--- bogo-sort/main.c
+++ bogo-sort/main.c
@@ -3,31 +3,20 @@
 #include <time.h>
 #include "func.h"
 
-int main()
-{
-  int len, from, to;
+int main(){
 
-  printf("Tamanho da lista: ");
-  scanf("%d", &len);
-  
+  // aqui comeca os experimentos
+  printf("Experimento 1:\n");
+  int len = 10;
   int lista[len];
-
-  printf("Gerar numeros aleatorios de: ");
-  scanf("%d", &from);
-  
-  printf("Ate: ");
-  scanf("%d", &to);
-
-  gerarListaAleatoria(lista, len, from, to+1);
+  gerarListaAleatoria(lista, len, 0, 10+1);
 
   printf("Lista Desordenada:\n");
-
   imprimeLista(lista, len);
 
   bogoSort(lista, len); // Chamada da função que aplica o metodo bogosort
 
   printf("\nLista Ordenada:\n");
-
   imprimeLista(lista, len);
 
   printf("\n");
