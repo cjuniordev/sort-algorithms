8426b2193c1efe36c796480636477b00b24b518a
cjuniordev <carlosrogeriocr02@gmail.com>
2022-01-03 17:23:00 -0300

    refactor: remove unecessary headers

--- bogo-sort/func.h
+++ bogo-sort/func.h
@@ -3,8 +3,6 @@
 #include <time.h>
 
 // cabeçalho das funções -----
-int estaOrdenado(int *lista, int n);
-void ordenar(int *lista, int n);
 void bogoSort(int *lista, int n);
 void imprimeLista(int *lista, int n);
 void gerarListaAleatoria(int *lista, int len, int m, int n);
--- radix-sort/func.h
+++ radix-sort/func.h
@@ -3,14 +3,6 @@
 #include <time.h>
 
 // cabeçalho das funções -----
-int pegaDigito(int numero, int divisor);
-int maximo(int lista[], int n);
-void juntaVetores(int *v, int *inicial, int *final, int n);
-void negativaLista(int *lista, int n);
-void inverteLista(int *lista, int n);
-void zeraLista(int *lista, int n);
-void retornaListaFinal(int *lista, int *temp, int n);
-void countingSort(int *lista, int n, int divisor, int *aux);
 void radixSort(int *lista, int n);
 void imprimeLista(int *lista, int n);
 void gerarListaAleatoria(int *lista, int len, int m, int n);
