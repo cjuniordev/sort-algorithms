84d628e5353a8e52d2fff9dfa4724924c28d4d59
cjuniordev <carlosrogeriocr02@gmail.com>
2022-01-04 14:04:20 -0300

    refactor: remove generate lists of timer

--- radix-sort/main.c
+++ radix-sort/main.c
@@ -9,56 +9,56 @@ int main()
   int *lista = (int *)calloc(len, sizeof(int));
   // exp 1
   clock_t tInicio;
-  tInicio = clock(); // inicia a marcação do tempo 
   gerarListaAleatoria(lista, len, 0, 10+1);
+  tInicio = clock(); // inicia a marcação do tempo 
   radixSort(lista, len); // Chamada da função que aplica o metodo radixSort
   printf("exp 1 | Tempo de execução: %.4fms\n", ((double)(clock() - tInicio) / (CLOCKS_PER_SEC / 1000)));
 
   // exp 2.1
   len = 1000;
   lista = (int *)calloc(len, sizeof(int));
-  tInicio = clock(); // inicia a marcação do tempo 
   gerarListaAleatoria(lista, len, -1000, 1000+1);
+  tInicio = clock(); // inicia a marcação do tempo 
   radixSort(lista, len); // Chamada da função que aplica o metodo radixSort
   printf("exp 2.1 | Tempo de execução: %.4fms\n", ((double)(clock() - tInicio) / (CLOCKS_PER_SEC / 1000)));
 
   // exp 2.2
   len = 1000;
   lista = (int *)calloc(len, sizeof(int));
-  tInicio = clock(); // inicia a marcação do tempo 
   gerarListaCrescente(lista, len);
+  tInicio = clock(); // inicia a marcação do tempo 
   radixSort(lista, len); // Chamada da função que aplica o metodo radixSort
   printf("exp 2.2 | Tempo de execução: %.4fms\n", ((double)(clock() - tInicio) / (CLOCKS_PER_SEC / 1000)));
 
   // exp 2.3
   len = 1000;
   lista = (int *)calloc(len, sizeof(int));
-  tInicio = clock(); // inicia a marcação do tempo 
   gerarListaDecrescente(lista, len);
+  tInicio = clock(); // inicia a marcação do tempo 
   radixSort(lista, len); // Chamada da função que aplica o metodo radixSort
   printf("exp 2.3 | Tempo de execução: %.4fms\n", ((double)(clock() - tInicio) / (CLOCKS_PER_SEC / 1000)));
 
   // exp 3.1
   len = 10000;
   lista = (int *)calloc(len, sizeof(int));
-  tInicio = clock(); // inicia a marcação do tempo 
   gerarListaAleatoria(lista, len, -99999, 99999+1);
+  tInicio = clock(); // inicia a marcação do tempo 
   radixSort(lista, len); // Chamada da função que aplica o metodo radixSort
   printf("exp 3.1 | Tempo de execução: %.4fms\n", ((double)(clock() - tInicio) / (CLOCKS_PER_SEC / 1000)));
 
   // exp 3.2
   len = 10000;
   lista = (int *)calloc(len, sizeof(int));
-  tInicio = clock(); // inicia a marcação do tempo 
   gerarListaCrescente(lista, len);
+  tInicio = clock(); // inicia a marcação do tempo 
   radixSort(lista, len); // Chamada da função que aplica o metodo radixSort
   printf("exp 3.2 | Tempo de execução: %.4fms\n", ((double)(clock() - tInicio) / (CLOCKS_PER_SEC / 1000)));
 
   // exp 3.3
   len = 10000;
   lista = (int *)calloc(len, sizeof(int));
-  tInicio = clock(); // inicia a marcação do tempo 
   gerarListaDecrescente(lista, len);
+  tInicio = clock(); // inicia a marcação do tempo 
   radixSort(lista, len); // Chamada da função que aplica o metodo radixSort
   printf("exp 3.3 | Tempo de execução: %.4fms\n", ((double)(clock() - tInicio) / (CLOCKS_PER_SEC / 1000)));
 
